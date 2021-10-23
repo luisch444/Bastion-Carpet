5aa4b64d46ce7e23cc982d2733259b8ca3523860
33234165+ManuDelAguila <33234165+ManuDelAguila@users.noreply.github.com>
2021-10-23 23:02:53 +0200

    Fix ZombifiedPiglinEntityMixin

--- src/main/java/carpet/bastion/mixin/ZombifiedPiglinEntityMixin.java
+++ src/main/java/carpet/bastion/mixin/ZombifiedPiglinEntityMixin.java
@@ -3,6 +3,7 @@ package carpet.bastion.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.ZombifiedPiglinEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -12,21 +13,23 @@ import org.spongepowered.asm.mixin.Mixin;
 import carpet.bastion.BastionCarpetSettings;
 
 @Mixin(ZombifiedPiglinEntity.class)
-public abstract class ZombifiedPiglinEntityMixin extends Entity{
+public abstract class ZombifiedPiglinEntityMixin extends MobEntity {
 
-    public ZombifiedPiglinEntityMixin(EntityType<?> type, World world) {
-        super(type, world);
+
+    protected ZombifiedPiglinEntityMixin(EntityType<? extends MobEntity> entityType, World world) {
+        super(entityType, world);
     }
 
-    public void onDeath(DamageSource source) {
-
-        if(BastionCarpetSettings.zombifiedpiglindropsgoldenapple){
+    @Override
+    protected void dropLoot(DamageSource source, boolean causedByPlayer)
+    {
+        super.dropLoot(source, causedByPlayer);
+        if (BastionCarpetSettings.zombifiedpiglindropsgoldenapple)
+        {
             int rand = (int) (Math.random()*100000+1);
             if(rand==444) {
-                ItemStack stack = new ItemStack(Items.ENCHANTED_GOLDEN_APPLE);
-                this.dropStack(stack);
+                this.dropStack(new ItemStack(Items.ENCHANTED_GOLDEN_APPLE));
             }
         }
     }
-
 }
