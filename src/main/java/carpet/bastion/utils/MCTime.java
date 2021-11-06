347c157bc9ac0dce0a91bc3a82b146a58a3010d9
kahzerx <kahzerx@tutanota.com>
2021-11-06 15:09:31 +0100

    hours & days

--- src/main/java/carpet/bastion/utils/MCTime.java
+++ src/main/java/carpet/bastion/utils/MCTime.java
@@ -2,7 +2,9 @@ package carpet.bastion.utils;
 
 public enum MCTime {
     SECONDS(20),
-    MINUTES(20 * 60);
+    MINUTES(20 * 60),
+    HOURS(20 * 60 * 60),
+    DAYS(20 * 60 * 60 * 24);
 
     private final int time;
 
