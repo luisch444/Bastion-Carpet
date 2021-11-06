113226227e78db8c359bc6602a9c7b94b7c1df5a
luisch444 <luisch2005@gmail.com>
2021-11-06 13:21:13 +0000

    commandAlert rule added

--- src/main/java/carpet/bastion/BastionCarpetSettings.java
+++ src/main/java/carpet/bastion/BastionCarpetSettings.java
@@ -25,4 +25,7 @@ public class BastionCarpetSettings {
     @Rule(desc = "Enables /signal command to get a container with comparator value.", category = { BASTION, CREATIVE, COMMAND })
     public static boolean commandSignal = false;
 
+    @Rule(desc = "Enables /alert command", category = { BASTION, COMMAND })
+    public static boolean commandAlert = false;
+
 }
